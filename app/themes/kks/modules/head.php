f1907f4c8d5b0508bf23f976eab725dd7bb921b0
Pontus Abrahamsson <pontus@lostisland.co>
2017-01-21 12:57:02 +0100

    Cleanup

--- app/themes/kks/modules/head.php
+++ app/themes/kks/modules/head.php
@@ -1,21 +1,17 @@
 <header>
-	<div>
+	<div class="logo">
 		<h1>
 			<a href="{{get_bloginfo('url')}}" id="logo">{{ get_bloginfo('name') }}</a>
 		</h1>
 	</div>
 
-	<div id="bar">
-		<div>
-			<nav>
-				<ul>
-					{{ wp_list_pages([
-						'depth' => 1,
-						'title_li' => '',
-						'walker' => new Core\Walker,
-					]) }}
-			</ul>
-		</nav>
-	</div>
-	</div>
+	<nav>
+		<ul>
+			{{ wp_list_pages([
+				'depth' => 1,
+				'title_li' => '',
+				'walker' => new Core\Walker,
+			]) }}
+		</ul>
+	</nav>
 </header>
