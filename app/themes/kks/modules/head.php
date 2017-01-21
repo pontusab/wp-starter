765367d7fd916b87bf22f40892306bcbcbd1a81e
Pontus Abrahamsson <pontus@lostisland.co>
2017-01-21 12:44:11 +0100

    Structure

--- app/themes/kks/modules/head.php
+++ app/themes/kks/modules/head.php
@@ -1,19 +1,8 @@
-{{{ $contact_id = Core\Frontend::get_id_by_template('contact') }}}
-
 <header>
 	<div>
 		<h1>
 			<a href="{{get_bloginfo('url')}}" id="logo">{{ get_bloginfo('name') }}</a>
 		</h1>
-		<ul class="links">
-			<li>
-				<a href="{{get_permalink($contact_id)}}">{{ get_the_title($contact_id) }}</a>
-			</li>
-
-			{{ Core\Frontend::language_switcher()}}
-			{{ Core\Frontend::init()->user_menu() }}
-
-		</ul>
 	</div>
 
 	<div id="bar">
@@ -24,12 +13,9 @@
 						'depth' => 1,
 						'title_li' => '',
 						'walker' => new Core\Walker,
-						'exclude' => Core\Frontend::get_posts_by_slug(true)
 					]) }}
 			</ul>
 		</nav>
-
-		@module('search-form')
 	</div>
 	</div>
 </header>
--- app/themes/kks/views/template-home.php
+++ app/themes/kks/views/template-home.php
@@ -1,25 +1,5 @@
 <?php // Template Name: Startsida ?>
 
-@layout('start layout-1')
+@layout('home')
 @module('doctype')
 @module('head')
-
-@module('hero')
-
-<div id="main">
-  <div class="content-1">
-    @module('latest-seminars')
-  </div>
-
-  <div class="content-2">
-    @module('latest-news')
-  </div>
-
-  <div class="content-3">
-    @module('profile')
-    @module('register')
-  </div>
-</div>
-
-@module('owners')
-@module('footer')
