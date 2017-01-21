20485363fcbaa4969c0f0674efa8c00ec4fbecc8
Pontus Abrahamsson <pontus@lostisland.co>
2017-01-21 12:35:36 +0100

    gitignore

--- config/local-temp.php
+++ config/local-temp.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_NAME', 'kks');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'WP@konooa');
-define('DB_HOST', '127.0.0.1');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 $table_prefix  = 'wp_';
