e2b4dd57d1ce354163b5cfc936e86e59fe69e795
Pontus Abrahamsson <pontus@lostisland.co>
2017-01-21 12:36:23 +0100

    Remove cache

--- app/themes/kks/.tmpl_cache/89f68858591a3b5e413a565cea1f774f_settings-updated.php
+++
@@ -1,9 +0,0 @@
-<?php if(isset($_GET['success']) && $_GET['success'] === 'true'): ?>
-  <div id="message" class="updated">
-    <p><?php echo pll__('settings_updated'); ?></p>
-  </div>
-<?php elseif(isset($_GET['success']) && $_GET['success'] === 'false'): ?>
-  <div id="message" class="updated error">
-    <p><?php echo pll__('settings_updated_error'); ?></p>
-  </div>
-<?php endif; ?>
--- app/themes/kks/.tmpl_cache/c9c7b111083bc6996a743b01c061b550_search-form.php
+++
@@ -1,4 +0,0 @@
-<form class="search" action="<?php echo pll_home_url(); ?>" method="GET">
-  <input type="text" name="s" value="<?php echo get_search_query(); ?>">
-  <input type="submit" value="<?php echo pll__('search'); ?>">
-</form>
