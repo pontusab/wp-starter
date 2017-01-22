097a9b8cba195db1da6a8472c88cc618cc4c8aa1
Pontus Abrahamsson <pontus@lostisland.co>
2017-01-23 00:12:09 +0100

    Strucure

--- wp-config.php
+++ wp-config.php
@@ -7,5 +7,5 @@ $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 // Load application and settings
-require_once(dirname(__FILE__ ) . '/config/app.php');
+require_once(dirname(__FILE__ ) . '/app.php');
 require_once(ABSPATH . 'wp-settings.php');
