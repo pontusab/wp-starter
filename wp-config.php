edd5ac8ff7a3763b1db18836b05431cd51d69939
Pontus Abrahamsson <pontus@lostisland.co>
2017-01-23 11:00:20 +0100

    Update config

--- wp-config.php
+++ wp-config.php
@@ -2,10 +2,6 @@
 // Load composer
 require_once(__DIR__ . '/vendor/autoload.php');
 
-// Load env-file
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
-
 // Load application and settings
-require_once(dirname(__FILE__ ) . '/app.php');
+require_once(dirname(__FILE__ ) . '/config/application.php');
 require_once(ABSPATH . 'wp-settings.php');
